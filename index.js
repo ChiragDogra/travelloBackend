b18f987bbc46f9fb581ab33d0c818476363b78f9
Chirag Dogra <chirag.dogra79@gmail.com>
2022-08-06 12:32:59 +0530

    following js mastery

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
@@ -18,7 +19,19 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
+app.get('/', (req, res)=>{
+    res.send('App is running')
+})
 
+// Serve static assests if we're in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
